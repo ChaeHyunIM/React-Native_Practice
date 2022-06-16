0c6e76c992beccb8006f95ae28d4edb7226e28bc
Youngbum <kimuyb@gmail.com>
2022-06-16 18:04:29 +0900

    add haptics, longpress handler

--- src/components/organisms/TimeTable.tsx
+++ src/components/organisms/TimeTable.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { createRef, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import styled from 'styled-components/native';
-import { Gesture, LongPressGestureHandler, ScrollView } from 'react-native-gesture-handler';
-import Animated, { runOnJS, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+import { LongPressGestureHandler, ScrollView } from 'react-native-gesture-handler';
+import Animated, { runOnJS, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 import * as Haptics from 'expo-haptics';
 
 const boxColor = '#52576222';
@@ -39,47 +39,9 @@ const TimeTable = () => {
   const isLongPressed = useSharedValue(false);
 
 
-  const longPress = Gesture.LongPress()
-    .onStart(e => {
-      isLongPressed.value = true;
-    });
 
 
-  const dragGesture = Gesture.Pan()
-    .simultaneousWithExternalGesture(longPress)
-    .manualActivation(true)
-    .onTouchesMove((event, stateManager) => {
-      if (isLongPressed.value) {
-        stateManager.activate();
-      } else {
-        stateManager.fail();
-      }
-    })
-    .onUpdate((e) => {
-      offset.value = {
-        x: e.translationX + start.value.x,
-        y: e.translationY + start.value.y,
-      };
-      endIndex.value = (Math.floor((e.y) / size) * 7 + Math.floor(e.x / size));
 
-    })
-    // .onEnd((e) => {
-    //   start.value = {
-    //     x: offset.value.x,
-    //     y: offset.value.y,
-    //   };
-    // })
-    .onTouchesUp((e) => {
-      endIndex.value = (Math.floor((e.y) / size) * 7 + Math.floor(e.x / size));
-      setNum(endIndex.value);
-      isLongPressed.value = false;
-    });
-  // .onFinalize(() => {
-  //   isPressed.value = false;
-  //   // startIndex.value=0
-  // });
-
-  const composed = Gesture.Simultaneous(dragGesture, longPress);
 
   const Box = ({ size = 44, innerText = '', index = 0, ...props }) => {
     // const [selected, setSelected] = useState(false);
@@ -88,13 +50,20 @@ const TimeTable = () => {
     const selfPosition = `[${row},${col}]`;
 
     const animatedStyles = useAnimatedStyle(() => {
-      runOnJS(Haptics.selectionAsync)();
+      if (isLongPressed.value) {
+        runOnJS(Haptics.selectionAsync)();
+      }
+      const isSelected = index <= endIndex.value && index >= startIndex.value;
+      const isStart = index === startIndex.value;
+      const isEnd = index === endIndex.value;
+
       return {
-        width, height, borderRadius,
+        width, height,
+        borderRadius,
         transform: [
           // { translateX: offset.value.x },
           // { translateY: offset.value.y },
-          // { scale: withSpring(isPressed.value ? 1.2 : 1) },
+          { scale: withSpring(isSelected ? 1.2 : 1) },
         ],
         backgroundColor: index < endIndex.value && index > startIndex.value ? 'gray' : '#FFF',
 
@@ -122,7 +91,6 @@ const TimeTable = () => {
     );
   };
 
-  const panGestureHandlerRef = createRef();
   const longPressGestureHandler = createRef();
 
 
@@ -130,12 +98,16 @@ const TimeTable = () => {
     <LongPressGestureHandler
       ref={longPressGestureHandler} maxDist={4000}
       onActivated={(e) => {
+        isLongPressed.value = true;
         startIndex.value = (Math.floor((e.nativeEvent.y) / size) * 7 + Math.floor(e.nativeEvent.x / size));
-
       }}
       onGestureEvent={(e) => {
         endIndex.value = (Math.floor((e.nativeEvent.y) / size) * 7 + Math.floor(e.nativeEvent.x / size));
-      }}>
+      }}
+      onEnded={() => {
+        isLongPressed.value = false;
+      }}
+    >
       <ScrollView style={{ width: '100%' }}
                   onLayout={onLayout}
                   contentContainerStyle={{ justifyContent: 'center', flexDirection: 'row', flexWrap: 'wrap' }}
